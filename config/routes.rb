7fb76bb4aa0ef317d04cb96eab0ce64f90a73b63
lu <lubiao.zc@gmail.com>
2017-01-02 14:55:06 +0800

    implement my post

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
     end
 
   resources :posts
-end
+  end
 
   namespace :account do
     resources :groups
+    resources :posts
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
